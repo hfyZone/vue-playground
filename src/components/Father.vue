60f64d8691396277d4c95b1ba5677f1d059d92b9
hfyZone <1178805691@qq.com>
2024-11-12 15:06:53 +0800

    vite init

---
+++ src/components/Father.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Son></Son>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import Son from "@/components/Son.vue";
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="son_wrapper">
+      <img src="@/assets/stewie.png" alt="STEWIE"/>
+      <div>
+        <h2>Hi,My name is Stewie!</h2>
+        <h3>My accounts: {{ account }}$</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      account: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+.son_wrapper {
+  background: aqua;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  gap: 20px;
+  justify-content: center;
+}
+.son_wrapper > img{
+  height: 200px;
+}
+</style>
